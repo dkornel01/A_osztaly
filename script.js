a6e37d587cdea43630621823e717a3c79d4a1c16
dkornel01 <115635103+dkornel01@users.noreply.github.com>
2023-03-20 17:22:42 +0100

    kesz

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const LIS=["kepek/kep1.jpg","kepek/kep2.jpg","kepek/kep3.jpg","kepek/kep4.jpg","kepek/kep5.jpg","kepek/kep6.jpg",];
 const KIVALASZTOTTKEPEK=[]
 let db=0;
-let szam=0;
+let szam=[0,1,2,3,4,5,6]
 
 $(function(){
     //ide jön a kód
     const BEIROELEM=$("#felso");
     const BEIROELEM2=$("#also");
-    let tarto=osszeAllit();
-    let tarto2=osszeAllit2();
+    let szamok=shuffel()
+    let tarto=osszeAllit(szamok);
+    szamok=shuffel()
+    let tarto2=osszeAllit2(szamok);
     BEIROELEM.append(tarto)
     BEIROELEM2.append(tarto2)
     const FELSOKEPEK=$("#felso img")
@@ -21,20 +23,20 @@ function kepreKattintas(){
    db++
    const ATKKEP=(event.target);
    console.log(db)
-   KIVALASZTOTTKEPEK.push($(ATKKEP).attr("id"))
    console.log($(ATKKEP).attr("id"))
    ATKKEP.src=LIS[ATKKEP.id]
+   KIVALASZTOTTKEPEK.push($(ATKKEP).attr("id"))
    if (db==2){
     db=0;
     visszaFordit()
    }
+   
 }
 
 function visszaFordit() {
     
     console.log(KIVALASZTOTTKEPEK);
     const FELSOKEPEK=$("#felso img");
-
     setTimeout(function(){
     let aktkepe=FELSOKEPEK.eq(KIVALASZTOTTKEPEK[0])
     $(aktkepe).attr("src", "kepek/hatter.jpg")
@@ -53,24 +55,48 @@ function visszaFordit() {
 
 }
 
-function osszeAllit(){
+function osszeAllit(szamok){
     let db=1;
+    szam.s
     let txt=""
     for (let index = 0; index < LIS.length; index++) {
-            txt+=`<div><img src="kepek/hatter.jpg"  alt="${db}" id="${szam}" ></div>`
+            txt+=`<div><img src="kepek/hatter.jpg"  alt="${db}" id="${szamok[index]}" ></div>`
             db=db+1;
             szam++
     }
     return txt
 }
 
-function osszeAllit2(){
+function osszeAllit2(szamok){
     let db=1;
     let txt=""
     for (let index = 0; index < LIS.length; index++) {
-            txt+=`<div><img src="kepek/hatter.jpg"  alt="${db}" id="${szam-6}" ></div>`
+            txt+=`<div><img src="kepek/hatter.jpg"  alt="${db}" id="${szamok[index]}" ></div>`
             db=db+1;
             szam++
     }
     return txt
+}
+
+function shuffel() {
+    let van
+    let meg=[]
+    let szamok
+    while (meg.length<6) {
+        szamok=Math.floor(Math.random()*6)
+        van=false
+        for (let index = 0; index < meg.length; index++) {
+            if (szamok==meg[index]){
+                van=true
+            }
+            
+        }
+        if (van==false)
+        {
+            meg.push(szamok)
+        }
+        
+    }
+    console.log(meg)
+    return meg
 }
